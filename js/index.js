3ce42818b16d19074625c8dbd5c100cfc53c6c13
Hemerson Jhonatan <hemerson.j.pereira@gmail.com>
2023-11-18 11:18:46 -0300

    correção

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 //Create
-document.addEventListener('DOMContentLoaded', function(){
-document.getElementById('save').addEventListener('click', save);})
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("save").addEventListener("click", save);
+});
 
 function save() {
-  let nome = document.getElementById('name').value;
-  let novaTarefa = document.getElementById('save').textContent;
-  localStorage.setItem('meusdados', novaTarefa);
-  var dados = localStorage.getItem('meusDados');
-  var dadosRecuperados = JSON.parse(dados);
-  console.log(dadosRecuperados.novaTarefa);
+    let nome = document.getElementById('name').value;
+    localStorage.setItem('meusdados', nome);
+    var dados = localStorage.getItem('meusdados');
 }
-
-
-
-
+function save(){
+    var dadosExistentesString = localStorage.getItem('meusdados')
+    var dadosExistente = [];
+if (dadosExistentesString){
+    dadosExistente = JSON.parse(dadosExistentesString)
+}
+var novasPessoas = document.getElementById('name').value;
+dadosExistente.push(novasPessoas)
+localStorage.setItem('meusdados', JSON.stringify(dadosExistente))
+console,log('Nomes Salvos', dadosExistente)
+}
