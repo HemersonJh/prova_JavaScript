3c5baa7cc4a3634bdd5075c6746f67392d918558
Hemerson Jhonatan <hemerson.j.pereira@gmail.com>
2023-11-18 10:56:24 -0300

    adicionando mais linhas de codigo

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
-let nome = document.getElementById('name').value;
-let salvar = document.getElementById('save').value;
+//Create
+document.addEventListener('DOMContentLoaded', function(){
+document.getElementById('save').addEventListener('click', save);})
+
+function save() {
+  let nome = document.getElementById('name').value;
+  let novaTarefa = document.getElementById('save').textContent;
+  localStorage.setItem('meusdados', novaTarefa);
+  var dados = localStorage.getItem('meusDados');
+  var dadosRecuperados = JSON.parse(dados);
+  console.log(dadosRecuperados.novaTarefa);
+}
+
+
+
+
